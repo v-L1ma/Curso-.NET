de5bd368818190887fea5870bbbc936592e4123a
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-05-09 20:02:12 -0300

    criando rotas de get by id e put

--- VideoGameWebApi/Controllers/VideoGameController.cs
+++ VideoGameWebApi/Controllers/VideoGameController.cs
@@ -42,6 +42,47 @@ namespace VideoGameWebApi.Controllers
             return Ok(videoGames);
         }
 
+        [HttpGet("{Id}")]
+        public ActionResult<VideoGame> GetVideoGameById(int Id)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == Id);
+            if (game is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(game);
+        }
+
+        [HttpPost]
+        public ActionResult<VideoGame> addVideoGame(VideoGame newGame)
+        {
+            if (newGame is null)
+            {
+                return BadRequest();
+            }
+
+            newGame.Id = videoGames.Max(g => g.Id) + 1;
+            videoGames.Add((newGame));
+            return CreatedAtAction(nameof(GetVideoGameById), new { Id = newGame.Id }, newGame);
+        }
+
+        [HttpPut("{Id}")]
+        public IActionResult UpdateVideoGame(int Id, VideoGame updatesGame)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == Id);
+            if (game is null)
+            {
+                return NotFound();
+            }
+            
+            game.Title = updatesGame.Title;
+            game.Platform = updatesGame.Platform;
+            game.Developer = updatesGame.Developer;
+            game.Publisher = updatesGame.Publisher;
+
+            return NoContent();
+        }
     }
     
 }
